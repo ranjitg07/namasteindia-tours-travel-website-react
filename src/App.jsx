6f886a23a0b9863c9ccf6a51aa6b248985f2f78c
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-07 15:46:40 +0530

    imported all the components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Middle from './Components/Middle/Middle'
+import Destinations from './Components/Destinations/Destinations'
+import Portfolio from './Components/Portfolio/Portfolio'
+import Reviews from './Components/Reviews/Reviews'
+import Questions from './Components/Questions/Questions'
+import Subscribe from './Components/Subscribe/Subscribe'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return (
     <div>
-
+      <Navbar />
+      <Home/>
+      <Middle/>
+      <Destinations/>
+      <Portfolio/>
+      <Reviews/>
+      <Questions/>
+      <Subscribe/>
+      <Footer/>
     </div>
   )
 }
