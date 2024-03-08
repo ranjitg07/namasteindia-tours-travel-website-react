bf619ff6cb391b1ff98e7379a5ed06ae782a84d9
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-08 13:07:27 +0530

    middle section done

--- src/Components/Middle/Middle.jsx
+++ src/Components/Middle/Middle.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './Middle.css'
+import React from "react";
+import "./Middle.css";
 
 const Middle = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="middle section">
+      <div className="secContainer container">
+        <div className="grid">
+          <span className="flex">
+            <h1>10</h1>
+            <p>World Of Experiences</p>
+          </span>
 
-export default Middle
+          <span className="flex">
+            <h1>2K+</h1>
+            <p>Fine Destinations</p>
+          </span>
+
+          <span className="flex">
+            <h1>10K+</h1>
+            <p>Customer Reviews</p>
+          </span>
+
+          <span className="flex">
+            <h1>4.8</h1>
+            <p>Overall Rating</p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Middle;
