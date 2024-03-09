bbe192df687c0b341a3f2c09ca90658656b7bf06
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-09 15:56:54 +0530

    small change

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Questions.css";
-import Accordian from "./Accordion";
+import Accordion from "./Accordion";
 
 const Questions = () => {
   const [active, setActive] = useState("How do I choose the right travel destination for me?");
@@ -12,7 +12,7 @@ const Questions = () => {
       <div className="secContainer grid">
         {/* we will import a component file from a diff file */}
         <div className="accordian grid">
-          <Accordian
+          <Accordion
             title="How do I choose the right travel destination for me?"
             desc="Consider your interests, budget, desired experiences, and the type of
                   environment you enjoy. Research destinations that align with your
@@ -20,20 +20,20 @@ const Questions = () => {
             active={active}
             setActive={setActive}
           />
-          <Accordian
+          <Accordion
             title="What are the best times to visit specific destibations?"
             desc="Research the climate, weather patterns, and peak tourist seasons of the destination you're interested in. Opt for the shoulder seasons when the weather is pleasant, and crowds are fewer, if possible."
             active={active}
             setActive={setActive}
           />
-          <Accordian
+          <Accordion
             title="How can I find budget-friendly travel options and deals?"
             desc="Look for travel deals, discounts on flights and accommodations, and consider using travel apps or websites that offer competitive prices. Being
             flexible with your travel dates can also help you find better deals."
             active={active}
             setActive={setActive}
           />
-          <Accordian
+          <Accordion
             title="What essential items should I pack for my adventure?"
             desc="Pack appropriate clothing, toiletries, travel documents (passport, visa, etc.), essential medications, and any specific gear needed for your adventure (e.g., hiking boots, snorkeling gear)."
             active={active}
