44fd1d6b4ca68d2326a6e5a7fcc0d73e11850f57
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-09 22:08:10 +0530

    updated responsiveness of Home section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,10 @@ import './Home.css'
 // Imported Assests
 import Video from '../../assets/bgVideo.mp4'
 import { AiOutlineSwapRight } from "react-icons/ai";
+import image1 from "../../assets/image1.jpg"
+import image2 from "../../assets/image2.jpg"
+import image3 from "../../assets/image3.jpg"
+import image4 from "../../assets/image4.jpg"
 
 const Home = () => {
   return (
@@ -22,11 +26,10 @@ const Home = () => {
         <div className="content">
           <h3>Popular Places</h3>
           <div className="images flex">
-             {/* <img src="" alt="" /> */}
-             {/* <img src="" alt="" /> */}
-             {/* <img src="" alt="" /> */}
-             {/* <img src="" alt="" /> */}
-             {/* <img src="" alt="" /> */}
+             <img src={image1} alt="PopularPlace1" />
+             <img src={image2} alt="PopularPlace2" />
+             <img src={image3} alt="PopularPlace3" />
+             <img src={image4} alt="PopularPlace4" />
           </div>
         </div>
       </div>
